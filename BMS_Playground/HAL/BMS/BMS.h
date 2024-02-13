5fc5d4c958e516c961571d5c0c0572500245ccbf
AhmedAlyEl-Ghannam <ahmedaly.g112@gmail.com>
2024-02-13 10:21:26 +0200

    BMS function prototypes are done-ish

--- BMS_Playground/HAL/BMS/BMS.h
+++ BMS_Playground/HAL/BMS/BMS.h
@@ -1,13 +1,13 @@
 #ifndef BMS_H_
 #define BMS_H_
 
-
 // bms_init()
 /*
 	input: no of cells
 	output: void
 	what does it do? initializes ADC pins + timer initializations
 */
+void BMS_voidInit(u8 copy_u8NumOfCells);
 
 // bms_read()
 /*
@@ -15,13 +15,17 @@
 	output: cell voltage (in mV)
 	what does it do? reads the value on ADC pin corresponding to cell no
 */
+u16 BMS_u16ReadVoltage(u8 copy_u8CellNum);
+
 
 // bms_disp()
 /*
 	input: cell no && cell reading (in V)
-	output: LCD stuff
+	output: LCD stuff (void)
 	what does it do? what do you think?! XDD
 */
+void BMS_voidDisplayReading(u8 copy_u8CellNum, u16 copy_u16CellVoltage);
+
 
 // bms_periodicDisp()
 /*
@@ -32,6 +36,8 @@
 		-> for loop to read every cell's voltage (use bms_read())
 		-> inside the same for loop display the reading of each cell
 */
+void BMS_voidPeriodicallyDisplayReading(u8 copy_u8NumOfCells);
+
 
 
 
